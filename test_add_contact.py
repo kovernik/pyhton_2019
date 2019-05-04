e3bd3ce87bfd13cfac8ae7050a5a0d6352787607
motekito <motekito@gmail.com>
2019-05-04 15:15:26 +0300

    homework_4

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,76 +1,27 @@
-from selenium import webdriver
 from contact import Contact
-import unittest
+from application_contact import Application
+import pytest
 
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-class test_add_contact(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Chrome()
-        self.wd.implicitly_wait(60)
-
-    def test_add_contact(self):
-        self.open_home_page()
-        self.login(username="admin", password="secret")
-        self.open_contact_page()
-        self.create_contact(Contact(firstname="Mikhail", lastname="Ko", nickname="motekito"))
-        self.logout()
-
-    def test_add_empty_contact(self):
-        self.open_home_page()
-        self.login(username="admin", password="secret")
-        self.open_contact_page()
-        self.create_contact(Contact(firstname="", lastname="", nickname=""))
-        self.logout()
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/")
-
-    def login(self, username, password):
-        wd = self.wd
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-    def open_contact_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("add new").click()
-
-    def create_contact(self, contact):
-        wd = self.wd
-        # fill contact form
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
-
-    def tearDown(self):
-        self.wd.quit()
+def test_add_contact(app):
+    app.open_home_page()
+    app.login(username="admin", password="secret")
+    app.open_contact_page()
+    app.create_contact(Contact(firstname="Mikhail", lastname="Ko", nickname="motekito"))
+    app.logout()
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_add_empty_contact(app):
+    app.open_home_page()
+    app.login(username="admin", password="secret")
+    app.open_contact_page()
+    app.create_contact(Contact(firstname="", lastname="", nickname=""))
+    app.logout()
 
