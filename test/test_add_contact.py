827badc1a709204df37cca4487e841fad215aa30
motekito <motekito@gmail.com>
2019-05-04 17:35:35 +0300

    homework_5

---
+++ test/test_add_contact.py
@@ -0,0 +1,23 @@
+from model.contact import Contact
+from fixture.application_contact import Application
+import pytest
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_add_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.create(Contact(firstname="Mikhail", lastname="Ko", nickname="motekito"))
+    app.session.logout()
+
+
+def test_add_empty_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.create(Contact(firstname="", lastname="", nickname=""))
+    app.session.logout()
+
---
+++ test/test_add_group.py
@@ -0,0 +1,22 @@
+from model.group import Group
+from fixture.application import Application
+import pytest
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_add_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.create(Group(name="name", header="header", footer="footer"))
+    app.session.logout()
+
+
+def test_add_empty_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.create(Group(name="", header="", footer=""))
+    app.session.logout()
