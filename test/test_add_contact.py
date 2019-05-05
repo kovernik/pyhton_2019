383802601a1782537032e5d114a67b27e201e350
motekito <motekito@gmail.com>
2019-05-05 11:13:39 +0300

    3,2

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,13 +2,9 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="Mikhail", lastname="Ko", nickname="motekito"))
-    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="", lastname="", nickname=""))
-    app.session.logout()
 
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,12 +2,8 @@ from model.contact import Contact
 
 
 def test_modify_contact_name(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(firstname="New firstname"))
-    app.session.logout()
 
 
 def test_modify_contact_lastname(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(lastname="New lastname"))
-    app.session.logout()
